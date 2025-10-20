ec1f6264bc9cda6f2e63bce7fce6f48fa98eb452
xxx2131N <165327222+xxx2131N@users.noreply.github.com>
2025-10-20 00:40:51 -0300

    se eimplemento el mobimiento con dificultad pero se areglo grasias a unos cambios en general del unity

--- Assets/Prefabs/Player/Scrips/PlayerMovement.cs
+++ Assets/Prefabs/Player/Scrips/PlayerMovement.cs
@@ -5,19 +5,28 @@ public class NewMonoBehaviourScript : MonoBehaviour
     private Vector3 fuerzaPorAplicar;
     private float tiempoUltimaFuerza;
     private float intervaloTimepo;
+
+    private Vector3 speed;
+    private IMovementStrategy movementStrategy;
     void Start()
     {
         fuerzaPorAplicar = new Vector3(0f, 0f, 300f);
         tiempoUltimaFuerza = 0f;
         intervaloTimepo = 2f;
+
+        speed = new Vector3(5f, 0f, 0f);
+        SetMovementStrategy(new MovimientoSuave());
     }
 
     void Update()
-
     {
-
+        MovePlayer();
     }
-    
+    public void MovePlayer()
+    {
+        movementStrategy.Move(transform, speed.x);
+    }
+
     void FixedUpdate()
     {
         tiempoUltimaFuerza += Time.fixedDeltaTime;
@@ -27,4 +36,10 @@ public class NewMonoBehaviourScript : MonoBehaviour
             tiempoUltimaFuerza = 0f;
         }
     }
+
+    public void SetMovementStrategy(IMovementStrategy strategy)
+    {
+        this.movementStrategy = strategy;
+    }
+    
 }
